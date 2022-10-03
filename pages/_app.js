115f7ade357ab1b757bec911add7cdb6cb23642c
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-10-03 15:38:03 +0700

    1 file modified, 1 added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import Login from './login';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../config/firebase';
+import Loading from '../components/Loading';
+import { useEffect } from 'react';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loggedInUser, loading, _error] = useAuthState(auth);
+
+  if (loading) return <Loading />;
+
+  if (!loggedInUser) return <Login />;
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
